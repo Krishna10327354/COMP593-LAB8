18bb38bf623e209c2964d57c8c31b1d1591ebe04
Krishna10327354 <krishna@flemingcollege.ca>
2024-07-27 18:38:44 -0400

    create_relationships.py

---
+++ create_relationships.py
@@ -0,0 +1,71 @@
+"""
+Description:
+ Creates the relationships table in the Social Network database
+ and populates it with 100 fake relationships.
+
+Usage:
+ python create_relationships.py
+"""
+import os
+import sqlite3
+from random import random, choice
+from faker import Faker
+
+# Determine the path of the database
+script_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(script_dir, 'social_network.db')
+
+def main():
+    create_relationships_table()
+    populate_relationships_table()
+    print("Table created")
+  
+
+def create_relationships_table():
+    """Creates the relationships table in the DB"""
+    with sqlite3.connect(db_path) as con:
+        cur = con.cursor()
+        relationship_table = """
+        CREATE TABLE IF NOT EXISTS relationships
+        (
+            id INTEGER PRIMARY KEY, 
+            person1_id INTEGER NOT NULL,
+            person2_id INTEGER NOT NULL,
+            relationship_type TEXT NOT NULL,
+            start_date DATE NOT NULL,
+            FOREIGN KEY (person1_id) REFERENCES people (id),
+            FOREIGN KEY (person2_id) REFERENCES people (id)
+        );
+       """
+        cur.execute(relationship_table)
+        con.commit()
+
+def populate_relationships_table():
+    """Adds 100 random relationships to the DB"""
+    with sqlite3.connect(db_path) as con:
+        cur = con.cursor()
+        add_relationship = """
+        INSERT INTO relationships
+        (
+            person1_id,
+            person2_id,
+            relationship_type,
+            start_date
+        )
+        VALUES (?, ?, ?, ?);
+        """
+        fake = Faker()
+        con.execute("BEGIN TRANSACTION")
+        for _ in range(100):
+            person1_id = fake.random_int(min=1, max=100)
+            person2_id = fake.random_int(min=1, max=100)
+            while person1_id == person2_id:
+                person2_id = fake.random_int(1, 100)
+            relationship_type = choice(['Friend', 'Spouse', 'Girlfriend'])
+            start_date = fake.date_between(start_date='-50y', end_date='today')
+            new_relationship = (person1_id, person2_id, relationship_type, start_date)
+            cur.execute(add_relationship, new_relationship)
+        con.commit()
+
+if __name__ == '__main__':
+     main()
